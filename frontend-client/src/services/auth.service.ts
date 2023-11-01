7cba3fadfe64eedd92fbb9b11b54b0127dae0468
catherinetkl <catherinetkll@gmail.com>
2023-11-01 18:51:40 +0800

    reconfigured app to run on java 17

--- frontend-client/src/services/auth.service.ts
+++ frontend-client/src/services/auth.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = `https://monorail.proxy.rlwy.net:11308/api/auth/`;
-// const API_URL = "http://localhost:8080/api/auth/";
+// const API_URL = `https://queryquay.up.railway.app:11308/api/auth/`;
+const API_URL = "http://localhost:8080/api/auth/";
 class AuthService {
     login(email: string, password: string) {
         return axios
--- jwtspringdemo/src/main/java/com/catherinetkl/spring/security/postgresql/security/WebSecurityConfig.java
+++ jwtspringdemo/src/main/java/com/catherinetkl/spring/security/postgresql/security/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -81,7 +82,7 @@ public class WebSecurityConfig {
                                 .requestMatchers("/api/test/**").permitAll()
                                 .anyRequest().authenticated()
                 )
-                .cors();
+                .cors(AbstractHttpConfigurer::disable);
 
         http.authenticationProvider(authenticationProvider());
 
