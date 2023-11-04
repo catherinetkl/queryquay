f6fa35cc33326140112dee5c3d345e08f0a06828
catherinetkl <catherinetkll@gmail.com>
2023-11-04 11:33:27 +0800

    changed application-prod properties

--- frontend-client/src/services/user.service.ts
+++ frontend-client/src/services/user.service.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'https://queryquay.up.railway.app:11308/api/test/';
-// const API_URL = 'http://localhost:8080/api/test/';
+// const API_URL = 'https://queryquay.up.railway.app:11308/api/test/';
+const API_URL = 'http://localhost:8080/api/test/';
 class UserService {
     getPublicContent() {
         return axios.get(API_URL + 'all');
